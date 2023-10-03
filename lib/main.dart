296721d65ae9d9867161efc2fc7e7dc0c963ceb0
Kal-Andre <andrewlou333@gmail.com>
2023-10-04 00:48:06 +0300

    A few color adjustments

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,9 @@ class _OnlineRadioAppState extends State<OnlineRadioApp> {
   late AudioPlayer audioPlayer;
   PlayerState playerState = PlayerState.STOPPED;
 
+  // Define variable for colour animation
+  Color backgroundColor = Colors.blueAccent; // Initial color
+  Color targetColor = Colors.lime; // Target color
   @override
   void initState() {
     super.initState();
@@ -23,6 +26,8 @@ class _OnlineRadioAppState extends State<OnlineRadioApp> {
     audioPlayer.onPlayerStateChanged.listen((state) {
       setState(() {
         playerState = state;
+        // Change the target color based on the player state
+        targetColor = (state == PlayerState.PLAYING) ? Colors.lime : Colors.blueAccent;
       });
     });
   }
@@ -46,9 +51,51 @@ class _OnlineRadioAppState extends State<OnlineRadioApp> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Online Radio"),
+        title: const Text("Online Radio")
       ),
-      body: Center(
+
+      // Wrap your content with an AnimatedContainer
+         body: AnimatedContainer(
+        duration: const Duration(milliseconds: 500), // Duration of the color transition
+        color: backgroundColor, // Set the background color
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                stationName,
+                style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                "Now Playing: $currentProgram",
+                style: const TextStyle(fontSize: 18),
+              ),
+              const SizedBox(height: 20),
+              IconButton(
+                icon: Icon(
+                  playerState == PlayerState.PLAYING
+                      ? Icons.pause
+                      : Icons.play_arrow,
+                  size: 48,
+                ),
+                onPressed: () {
+                  playOrPause();
+                  setState(() {
+                    // Update the background color when the button is pressed
+                    backgroundColor = targetColor;
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+      /*body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
@@ -77,10 +124,11 @@ class _OnlineRadioAppState extends State<OnlineRadioApp> {
       ),
     );
   }
-}
+}*/
 
 void main() {
   runApp(const MaterialApp(
     home: OnlineRadioApp(),
+    debugShowCheckedModeBanner: false,
   ));
 }
